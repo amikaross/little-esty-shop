59821044465a039a69090a769d53c1b5785a6570
Amanda Ross <amikaross@gmail.com>
2022-11-15 06:36:52 -0700

    Test: add test for can_be_applied validation method for bulk_discounts

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe BulkDiscount, type: :model do
     it { should validate_presence_of :discount }
     it { should validate_presence_of :threshold }
   end
+
+  describe "instance methods" do 
+    describe "#can_be_applied" do 
+      it "adds the appropriate error if the proposed discount will never be applied" do 
+        merchant = Merchant.create!(name: "I'm a merchant")
+        bulk_discount_1 = BulkDiscount.create!(discount: 20, threshold: 10, merchant: merchant)
+        bulk_discount_2 = BulkDiscount.new(discount: 15, threshold: 15, merchant: merchant)
+
+        expect(bulk_discount_2.valid?).to eq(false)
+        expect(bulk_discount_2.errors.messages).to eq({:discount=>["is less than the current highest discount, but with a greater threshold, and will never be applied"]})
+      end
+    end
+  end
 end
